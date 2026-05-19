d8c1f3f757f63404cb8775bb1a36a3cd6534b69f
abdelrhman-sd <abdelrhmansaeed001@gmail.com>
2026-05-19 17:55:04 +0300

    feat: added env vars loading

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import TransformResponseInterceptor from './interceptors/transform-response.interceptor';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+
   providers: [
     { provide: APP_INTERCEPTOR, useClass: TransformResponseInterceptor },
-    { provide: APP_FILTER, useClass: HttpExceptionFilter }
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ]
+
 })
 export class AppModule { }
